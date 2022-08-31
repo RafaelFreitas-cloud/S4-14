984fe8523e987c0925029cdb56a1b2bb363b30e9
RafaelFreitas <raf.mec.ba@gmail.com>
2022-08-31 10:16:27 -0300

    S4-14 final

---
+++ script.js
@@ -0,0 +1,42 @@
+// -----------------Exercício 1---------------------------
+// const tiposDiversos =[1.5,"String",false,10]
+// const tiposDiversos2 =[true,"String","boolen",'10']
+  
+// function qualTipoDeElemento(lista){
+//     for (let i=0; i<lista.length; i++){
+//         if (typeof(lista[i]) == "number"){
+//             return true
+//         }  
+            
+//      }
+//         return false
+//     }
+    
+    
+
+// console.log(qualTipoDeElemento(tiposDiversos))
+// console.log(qualTipoDeElemento(tiposDiversos2))
+
+
+// -----------------Exercício 2---------------------------
+let passageiros = ["rafael","almeida","freitas"]
+
+
+let resultado=Math.floor(Math.random() * (4 - 1) + 1)
+// console.log(Math.floor(Math.random() * (4 - 1) + 1))
+// console.log(resultado)
+
+console.log(passageiros[resultado-1])
+
+
+// -----------------Exercício Smilly---------------------------
+// let arr=[2,"rafael", true, 1.8]
+
+// function imprimir (lista){
+//     for (i=0 ; i<lista.length; i++){
+//         console.log(lista[i])
+//         console.log(typeof(lista[i]))
+//     }
+// }
+
+// imprimir(arr)
